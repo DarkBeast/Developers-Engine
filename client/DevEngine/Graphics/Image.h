b37413d1969ed8201ddac51628b1b2f2585bc776
genusistimelord <lordsatin@hotmail.com>
2012-09-26 15:17:11 -0400

    updated Image header temporarily for testing purposes only.

--- client/DevEngine/Graphics/Image.h
+++ client/DevEngine/Graphics/Image.h
@@ -1,6 +1,9 @@
 #ifndef _image_H
 #define _image_H
 
-void read_png_file(char* file_name);
+#include <glfw.h>
+
+int read_png_file(char* file_name);
+GLuint png_texture_load(const char* file_name, int width, int height);
 void write_png_file(char* file_name);
 #endif
