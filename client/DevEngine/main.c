e905581b07d78733c44a2b4d25b2909b034a0e36
Genusis <lordsatin@hotmail.com>
2012-10-08 20:06:27 -0400

    started setting up test for Rendering.

--- client/DevEngine/main.c
+++ client/DevEngine/main.c
@@ -6,62 +6,24 @@
 #include "image.h"
 #include "program_path.h"
 #include "Error.h"
-
+#include "Types.h"
 
 
 int main( )
 {
 	int running = GL_TRUE;
-
+	Image img;
 
 	CDLGetCurrentDirectory();
-
-
-
-	// Initialize GLFW
-	if( !glfwInit() )
-		RenderError(GLFWINIT_ERROR);
-
-
-	// Finally we can Open an OpenGL window
-	if(!glfwOpenWindow ( 1024, 768, 1,0,0,0,0,0, GLFW_WINDOW ))
-		RenderError(GLFWWIN_ERROR);
-
-
-	glfwSetWindowTitle( TITLE );//Sets the Windows Name
-
 
 	// Main loop
 	while( running )
 	{
-
-		//Clear information from last draw
-
-		glClear( GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
-		glPushAttrib(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
 
 
-		glMatrixMode(GL_MODELVIEW); //Switch to the drawing perspective
-
-		glLoadIdentity(); //Reset the drawing perspective
-
-
-
-		glBegin(GL_QUADS);
-
-		glVertex3f(-0.4, 0.4, -1);
-		glVertex3f( 0.4, 0.4, -1);
-		glVertex3f( 0.4,-0.4, -1);
-		glVertex3f(-0.4,-0.4, -1);
-		glEnd();
-
-
-
-		//glDisable(GL_TEXTURE_2D);
-		glPopAttrib();
-
+        //Clear information from last draw
 		glFlush();
-
 		glfwSwapBuffers();
 		// Check if ESC key was pressed or window was closed
 		running = glfwGetWindowParam( GLFW_OPENED );
