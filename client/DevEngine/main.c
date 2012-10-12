953cfdef53a7e6519fc37b3cebcf3d834fda5568
Genusis <lordsatin@hotmail.com>
2012-10-11 20:11:22 -0400

    added in test

--- client/DevEngine/main.c
+++ client/DevEngine/main.c
@@ -7,21 +7,34 @@
 #include "program_path.h"
 #include "Error.h"
 #include "Types.h"
-
+#include "Render.h"
 
 int main( )
 {
 	int running = GL_TRUE;
 	Image img;
-
+	SCoord pos;
 	CDLGetCurrentDirectory();
-
+	InitScreen(800,600,GLFW_WINDOW);
+	
+	glfwSetWindowSizeCallback(handleResize); 
 	// Main loop
 	while( running )
 	{
-		
+		glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
 
+		if(LoadImage( GetPath("pngtest.png"), &img, 0))
+		{
+			RenderError(IMAGELOADER_ERROR);
+		}
 
+		DrawStateReset();
+		pos.x1 = 0;
+		pos.x2 = 0;
+		pos.y1 = 0;
+		pos.y2 = 0;
+
+		Draw(&img, pos);
         //Clear information from last draw
 		glFlush();
 		glfwSwapBuffers();
