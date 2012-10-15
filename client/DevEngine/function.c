6c5ab693e44a53ef7eabf2d8e09b47e3bd7daae7
Genusis <lordsatin@hotmail.com>
2012-10-15 17:13:50 -0400

    added function next power of two

---
+++ client/DevEngine/function.c
@@ -0,0 +1,13 @@
+
+#include "function.h"
+
+size_t next_power_of_two(size_t input) {
+    size_t index;
+    
+    --input;
+    
+    for (index = 1; index < sizeof(size_t); index <<= 1)
+        input |= input >> index;
+    
+    return ++input;
+}
