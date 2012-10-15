8dfdac5d91b2b44eec8cbb5a7659d5aa1b1378a4
Genusis <lordsatin@hotmail.com>
2012-10-15 17:05:50 -0400

    added new types and changed names

--- client/DevEngine/Types.h
+++ client/DevEngine/Types.h
@@ -1,52 +1,78 @@
 #ifndef _TYPES_H
 #define _TYPES_H
 
-#include "integer.h"
 #include <glfw.h>
+#include "integer.h"
 
 typedef struct Screen
 {
 	int Width;
 	int Height;
-
 }Screen;
 
-typedef struct SCoord
+typedef struct Color
 {
-	int x1;
-	int x2;
-	int y1;
-	int y2;
-}SCoord;
+	unsigned int r;
+	unsigned int g;
+	unsigned int b;
+	unsigned int a;
+}Color;
 
-typedef struct Coords
+typedef struct Vector2i // the coords for placing on screen
+{
+	int x;
+	int y;
+}Vector2i;
+
+typedef struct Vector2ui // the coords for placing on screen
+{
+	unsigned int x;
+	unsigned int y;
+}Vector2ui;
+
+typedef struct Vector2f
+{
+    float x;
+    float y;
+}Vector2f;
+
+typedef struct Vector3i //for actule tile coords
 {
 	int y;
 	int x;
 	int z;
-}Coords;
+}Vector3i;
 
-typedef struct FCoords
+typedef struct Vector3ui //for actule tile coords
+{
+	unsigned int y;
+	unsigned int x;
+	unsigned int z;
+}Vector3ui;
+
+typedef struct Vector3f // for pixels
 {
 	float y;
 	float x;
 	float z;
-}FCoords;
+}Vector3f;
 
-typedef struct Vertex 
+typedef struct Vertex  
 {
-	Coords position;
-	Coords TexPos;
-
+	Vector2i position;
+	Vector2f texpos;
+	Color color;
 }Vertex;
 
 typedef struct Image
 {
-	int Width, Height;
-	int Format;
-	int BytesPerPixel;
-	unsigned char *pixels;
-	GLuint texID;
+	int Width; // le Width
+	int Height; //le height
+	int Format; // the format of the image.
+	int BytesPerPixel; //how many bytes per pixel are in the image
+	unsigned char *pixels; //actule pixel data of image
+	char reload;
+	GLuint texID; //image ID in opengl
 }Image;
 
 #endif
