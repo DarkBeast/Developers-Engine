919e501ff459ea3b3057924c85677ca32838c5bb
Genusis <lordsatin@hotmail.com>
2012-10-15 17:08:50 -0400

    fixed formatting

--- client/DevEngine/Keys.c
+++ client/DevEngine/Keys.c
@@ -1,12 +1,13 @@
-#include "Keys.h"
-#include <stdlib.h>
+
 #include <glfw.h>
+#include <stdlib.h>
+#include "Keys.h"
 
 void GLFWCALL handleKeypress(int key,int press) //The key that was pressed
 {
 	switch (key) {
-		case GLFW_KEY_ESC: //Escape key
-			exit(0); //Exit the program
+	case GLFW_KEY_ESC: //Escape key
+		exit(0); //Exit the program
 	}
 }
 
--- client/DevEngine/Keys.h
+++ client/DevEngine/Keys.h
@@ -1,9 +1,6 @@
 #ifndef _KEYS_H
 #define _KEYS_H
 
-
-
-
-
+void GLFWCALL handleKeypress(int key,int press);
 
 #endif
