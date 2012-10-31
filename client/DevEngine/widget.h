c55769ba8e160ad56b77fe717fda38f7617c10d4
Genusis <lordsatin@hotmail.com>
2012-10-30 20:10:15 -0400

    changed wector2f in widgets to vector2i, no need for floats. Added Clicked to canvas for mouse is or is not clicked and which button it is. removed widgetarray redid old widget functions Added public *root and *screen =].

--- client/DevEngine/widget.h
+++ client/DevEngine/widget.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Credits:  Andrew Wheeler/Genusis    
+* Credits:  Andrew Wheeler/Genusis
 ******************************************************************************/
 
 #ifndef _UI_H
@@ -8,7 +8,7 @@
 #include "types.h"
 #include "controls.h"
 
-typedef struct 
+typedef struct
 {
 	void *type;
 	void(*draw)(void *);
@@ -18,40 +18,44 @@ typedef struct
 	void(*keypressed)(void *,int,int);
 	voidarray widgets;
 	voidarray hidden;
+	void *parent;
 	vector2i pos;
-	vector2f imgpos; 
-	image image;
-	int width; 
+	vector2i imgpos;
+	image img;
+	int width;
 	int height;
 	char visisble;
-    char canuse;
-    char draggable;
+	char canuse;
+	char draggable;
 } widget;
 
-
-typedef struct 
+typedef struct
 {
 	vector2i mouseclick;
 	vector2i mousepos;
-	uint16 focusedcontrol; //can hold any control # up to 65,535 
+	uint16 focusedcontrol; //can hold any control # up to 65,535
+	uint8 clicked;
 }canvas;
 
+void widgetinit(canvas *screen, widget *parent);//see function for details
+void switchwidget(voidarray *wgt, size_t a, size_t b);
+void hidewidget(widget *parent, size_t index);
+void showwidget(widget *parent, size_t index);
+void addtowidget(widget *parent, widget *child, char hidden);
+void initwidget(widget *wgt);
+char clearbothwidgetarrays(widget *parent);
+char clearhiddenarray(widget *parent);
+char clearshownarray(widget *parent);
+void initwidgetarray(widget *parent, char opt);
+void widgetarrayresize(widget *parent, char opt, size_t size);//for dynamic widget arrays, the system currently uses static.
+void unloadwidget(widget *parent, size_t index);
 
-typedef struct 
-{
-	widget **data;
-	size_t size;
-	size_t count;
-}widgetarray;
-
-void initwidget(void  *wgt);
-void AddtoWidget(void  *wgt,void *sub);
-void switchwidget(void  *wgt, int a, int b);
-
-void initmousepress(void *wgt, int button, int pressed);//starter types, leave blank to prevent error on widget creation.
+void initmousepress(void *wgt, int button, int pressed);
 void initmouserelease(void *wgt, int button, int pressed);
 void initmousewheel(void *wgt, int moved);
 void initkeypressed(void *wgt, int key, int pressed);
-void initdraw(void *wgt);// end of starter types
+void initdraw(void *wgt);
 
+widget *root;
+canvas *screen;
 #endif
