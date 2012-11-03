d518c8b7d39b7c44edafe0c48a1857181ecd3468
Genusis <lordsatin@hotmail.com>
2012-11-03 19:05:39 -0400

    fixed spelling XD

--- client/DevEngine/Error.c
+++ client/DevEngine/Error.c
@@ -16,34 +16,34 @@ void fatalerror(int error_flag)
 	switch(error_flag)
 	{
 	case MISC_ERROR:
-		fputs("An error has occured!\n", stderr);
+		fputs("An error has occurred!\n", stderr);
 		break;
 	case POINTER_IN_USE:
-		fputs("An error has occured: Pointer in use!\n", stderr);
+		fputs("An error has occurred: Pointer in use!\n", stderr);
 		break;
 	case UNDEFINED_POINTER:
-		fputs("An error has occured: Undefined Pointer!\n", stderr);
+		fputs("An error has occurred: Undefined Pointer!\n", stderr);
 		break;
 	case OUT_OF_MEMORY:
-		fputs("An error has occured: Server Out Of Memory!\n", stderr);
+		fputs("An error has occurred: Server Out Of Memory!\n", stderr);
 		break;
 	case IO_ERROR:
-		fputs("An error has occured: I/O Error!\n", stderr);
+		fputs("An error has occurred: I/O Error!\n", stderr);
 		break;
 	case FILE_ERROR:
-		fputs("An error has occured: File Error!\n", stderr);
+		fputs("An error has occurred: File Error!\n", stderr);
 		break;
 	case OVERFLOW_ERROR:
-		fputs("An error has occured: Overflow!\n", stderr);
+		fputs("An error has occurred: Overflow!\n", stderr);
 		break;
 	case SOCKET_ADDR_ERROR:
 		fputs("Could not establish server address!\n", stderr);
 		break;
 	default:
-		fputs("An error has occured!\n", stderr);
+		fputs("An error has occurred!\n", stderr);
 	}
 	fputs("The Application will now close.", stderr);
-	getch();
+	_getch();
 	shutdown();
 }
 
@@ -52,44 +52,44 @@ void rendererror(int error_flag)
 	switch(error_flag)
 	{
 	case MISC_ERROR:
-		fputs("An error has occured!\n", stderr);
+		fputs("An error has occurred!\n", stderr);
 		break;
 	case POINTER_IN_USE:
-		fputs("An error has occured: Pointer in use!\n", stderr);
+		fputs("An error has occurred: Pointer in use!\n", stderr);
 		break;
 	case UNDEFINED_POINTER:
-		fputs("An error has occured: Undefined Pointer!\n", stderr);
+		fputs("An error has occurred: Undefined Pointer!\n", stderr);
 		break;
 	case OUT_OF_MEMORY:
-		fputs("An error has occured: Server Out Of Memory!\n", stderr);
+		fputs("An error has occurred: Server Out Of Memory!\n", stderr);
 		break;
 	case IO_ERROR:
-		fputs("An error has occured: I/O Error!\n", stderr);
+		fputs("An error has occurred: I/O Error!\n", stderr);
 		break;
 	case FILE_ERROR:
-		fputs("An error has occured: File Error!\n", stderr);
+		fputs("An error has occurred: File Error!\n", stderr);
 		break;
 	case OVERFLOW_ERROR:
-		fputs("An error has occured: Overflow!\n", stderr);
+		fputs("An error has occurred: Overflow!\n", stderr);
 		break;
 	case GLFWINIT_ERROR:
-		fputs("An error has occured: GLFW Did Not Intalize!\n", stderr);
+		fputs("An error has occurred: GLFW Did Not Intalize!\n", stderr);
 		break;
 	case GLFWWIN_ERROR:
-		fputs("An error has occured: GLFW Did Not Open Window!\n", stderr);
+		fputs("An error has occurred: GLFW Did Not Open Window!\n", stderr);
 		break;
 	case IMAGELOADER_ERROR:
-		fputs("An error has occured: Image Did Not Load!\n", stderr);
+		fputs("An error has occurred: Image Did Not Load!\n", stderr);
 		break;
 	default:
-		fputs("An error has occured!\n", stderr);
+		fputs("An error has occurred!\n", stderr);
 	}
 	fputs("The Application will now close.", stderr);
-	getch();
+	_getch();
 	shutdown();
 }
 
-void shutdown()
+void shutdown(void)
 {
 	//TODO: Clean up memory, shut down sockets, etc....?
 	glfwTerminate();
