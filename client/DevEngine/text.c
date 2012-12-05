3e6d76906feeab1f43f8f76fa31b7bdc0af65a87
Genusis <lordsatin@hotmail.com>
2012-12-04 19:21:00 -0500

    crap font rendering and glyph cache/failure so far XD. originally will work fine without cache need to Work on and fix cache for Super fast text drawing =].

--- client/DevEngine/text.c
+++ client/DevEngine/text.c
@@ -1,16 +1,139 @@
+//ignore this for now its crap.
+
 /*******************************************************************************
 * Credits:  Andrew Wheeler/Genusis
 *           Freetype library 2
 ******************************************************************************/
 #include "text.h"
+#include "function.h"
 #include "globals.h"
 #include "program_path.h"
 #include "render.h"
 #include <glfw.h>
+#include <string.h>
 
+#define MAXWIDTH 1024
 fonts font;
 
-void initfont(const char *fontname)
+atlas *a48;
+
+void initcache(atlas *cache)
+{
+	cache = (atlas *)calloc(1,sizeof(atlas));
+	memset(cache->character, NULL, sizeof cache->character);
+	cache->height = 0;
+	cache->width = 0;
+	cache->tex = NULL;
+}
+
+void initatlas(FT_Face face, int height, atlas *cache) {
+	FT_GlyphSlot g = face->glyph;
+
+	int i = 0;
+	int ox = 0;
+	int oy = 0;
+	int roww = 0;
+	int rowh = 0;
+	printf("started initlizing text");
+	initcache(cache);
+	//memset(arr, NULL, sizeof arr);
+	FT_Set_Pixel_Sizes(face, 0, height);
+	/* Find minimum size for a texture holding all visible unicode characters */
+	for (i = 0; i <= 0x0010FFFF; i++) {
+		if (FT_Load_Char(face, i, FT_LOAD_RENDER)) {
+			fprintf(stderr, "Loading character %c failed!\n", i);
+			continue;
+		}
+		if (roww + g->bitmap.width + 1 >= MAXWIDTH) {
+			cache->width = max(cache->width, roww);
+			cache->height += rowh;
+			roww = 0;
+			rowh = 0;
+		}
+		roww += g->bitmap.width + 1;
+		rowh = max(rowh, g->bitmap.rows);
+	}
+
+	cache->width = max(cache->width, roww);
+	cache->height += rowh;
+
+	/* Create a texture that will be used to hold all ASCII glyphs */
+	glGenTextures(1, &cache->tex);
+	glBindTexture(GL_TEXTURE_2D, cache->tex);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, cache->width, cache->height, 0, GL_ALPHA, GL_UNSIGNED_BYTE, 0);
+
+	/* We require 1 byte alignment when uploading texture data */
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	/* Clamping to edges is important to prevent artifacts when scaling */
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	/* Linear filtering usually looks best for text */
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	//get the bitmap. pixels of the glyph.
+	rowh = 0;
+	printf("50% done");
+	for (i = 0; i < 0x0010FFFF; i++) {
+		if (FT_Load_Char(face, i, FT_LOAD_RENDER)) {
+			fprintf(stderr, "Loading character %c failed!\n", i);
+			continue;
+		}
+		g = face->glyph;
+		if (ox + g->bitmap.width + 1 >= MAXWIDTH) {
+			oy += rowh;
+			rowh = 0;
+			ox = 0;
+		}
+
+		glGenTextures(1, &cache->character[i].tex);
+		glBindTexture(GL_TEXTURE_2D, cache->character[i].tex);
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+		glTexSubImage2D(GL_TEXTURE_2D, 0, ox, oy, g->bitmap.width, g->bitmap.rows, GL_ALPHA, GL_UNSIGNED_BYTE, g->bitmap.buffer);
+
+		/* Clamping to edges is important to prevent artifacts when scaling */
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		/* Linear filtering usually looks best for text */
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		cache->character[i].advancex = g->advance.x >> 6;
+		cache->character[i].advancey = g->advance.y >> 6;
+
+		cache->character[i].image.width = g->bitmap.width;
+		cache->character[i].image.rows = g->bitmap.rows;
+
+		cache->character[i].bitmapleft = g->bitmap_left;
+		cache->character[i].bitmaptop = g->bitmap_top;
+
+		cache->character[i].offsetx = ox / cache->width;
+		cache->character[i].offsety = oy / cache->height;
+
+		rowh = max(rowh, g->bitmap.rows);
+		ox += g->bitmap.width + 1;
+	}
+	printf("100% done");
+	//finally set the textures for quick drawing.
+	//for (i = 0; i < 0x0010FFFF; i++) {
+	//	glGenTextures(1, &cache->character[i].tex);
+	//	glBindTexture(GL_TEXTURE_2D, cache->character[i].tex);
+
+	/* We require 1 byte alignment when uploading texture data */
+	//	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	//	glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, cache->character[i].image.width, cache->character[i].image.rows, 0, GL_ALPHA, GL_UNSIGNED_BYTE, cache->character[i].image.buffer);
+
+	/* Linear filtering usually looks best for text */
+	//	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+	//	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+	//}
+}
+
+void initfont(char *fontname)
 {
 	if(FT_Init_FreeType(&font.library))
 	{
@@ -30,56 +153,40 @@ void initfont(const char *fontname)
 	{
 		fprintf(stderr, "Could not open font %s\n", font.fontfilename);
 	}
+
+	a48  = (atlas *)calloc(1,  sizeof (atlas));
+	//a24  = (atlas *)calloc(1,  sizeof (atlas));
+	//a12  = (atlas *)calloc(1,  sizeof (atlas));
+
+	initatlas(font.face, 48, a48);
+	//initatlas(font.face, 24, a24);
+	//initatlas(font.face, 12, a12);
 }
 
 void rendertext(text *str)
 {
-	const char *p;
+	char *p;
 	uint16 x, y;
 	uint16 x2, y2, w, h;
-	FT_GlyphSlot g = font.face->glyph;
 	x = str->xy.x;
 	y = str->xy.y;
 
 	/* Create a texture that will be used to hold one "glyph" */
 
-	FT_Set_Char_Size( font.face, /* handle to face object */
-		0, /* char_width in 1/64th of points */
-		str->size*64, /* char_height in 1/64th of points */
-		300, /* horizontal device resolution */
-		300 ); /* vertical device resolution */
-	glGenTextures(1, &font.tex);
+	//FT_Set_Char_Size( font.face, /* handle to face object */
+	//	0, /* char_width in 1/64th of points */
+	//	str->size*64, /* char_height in 1/64th of points */
+	//	300, /* horizontal device resolution */
+	//	300 ); /* vertical device resolution */
 
-	glBindTexture(GL_TEXTURE_2D, font.tex);
-
-	/* We require 1 byte alignment when uploading texture data */
-
-	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-	/* Clamping to edges is important to prevent artifacts when scaling */
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-	/* Linear filtering usually looks best for text */
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	/* Loop through all characters */
+	glEnable (GL_TEXTURE_2D);
 	glColor4f(str->col.r, str->col.g, str->col.b, str->col.a);// set the image color properties, 1 being highest 0.0000 being lowest
 
-	for(p = str->text; *p; p++) {
+	for(p = str->data; *p; p++) {
 		/* Try to load and render the character */
 
-		if(FT_Load_Char(font.face, *p, FT_LOAD_RENDER))
-
-			continue;
-
-		/* Upload the "bitmap", which contains an 8-bit grayscale image, as an alpha texture */
-
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, g->bitmap.width, g->bitmap.rows, 0, GL_ALPHA, GL_UNSIGNED_BYTE, g->bitmap.buffer);
+		glBindTexture(GL_TEXTURE_2D, a48->character[*p].tex);
 
 		/* Calculate the vertex and texture coordinates */
 
@@ -87,9 +194,9 @@ void rendertext(text *str)
 
 		y2 = y ;
 
-		w = g->bitmap.width ;
+		w = a48->character[*p].image.width ;
 
-		h = g->bitmap.rows;
+		h = a48->character[*p].image.rows;
 
 		glBegin (GL_TRIANGLE_STRIP);
 		glTexCoord2i (1, 1);
@@ -106,12 +213,59 @@ void rendertext(text *str)
 
 		/* Advance the cursor to the start of the next character */
 
-		x += (g->advance.x >> 6) * str->hw.x;
+		x += (a48->character[*p].advancex >> 6) * str->hw.x;
 
-		y += (g->advance.y >> 6) * str->hw.y;
+		y += (a48->character[*p].advancey >> 6) * str->hw.y;
 	}
 }
 
-void inittext()
+void render_text(text *str, atlas *cache)
 {
+	int x, y,w, h,x2,x1,y2,y1;
+	char *p;
+	//x1 = imgpos.x / img->width;
+	//x2 = (imgpos.x + width) / img->width;
+	//y1 = imgpos.y / img->height;
+	//y2 = (imgpos.y +height) / img->height;
+	x = str->xy.x;
+	y = str->xy.y;
+	glBindTexture(GL_TEXTURE_2D, cache->tex);
+	for(p = str->data; *p; p++) {
+		x2 = x ;
+
+		y2 = y ;
+
+		w = cache->character[*p].image.width * str->xy.x;
+
+		h = cache->character[*p].image.rows * str->xy.y;
+
+		//float x2 =  x + cache->c[*p].bl * sx;
+		// float y2 = -y - cache->c[*p].bt * sy;
+		// float w = cache->c[*p].bw * sx;
+		// float h = cache->c[*p].bh * sy;
+
+		/* Advance the cursor to the start of the next character */
+		x += cache->character[*p].advancex * str->xy.x;
+		y += cache->character[*p].advancey * str->xy.y;
+
+		/* Skip glyphs that have no pixels */
+		if(!w || !h)
+		{
+			continue;
+		}
+
+		glBegin (GL_TRIANGLE_STRIP);
+		glTexCoord2i (1, 1);
+		glVertex2i (x2 + w, y2 + h);
+
+		glTexCoord2i (1, 0);
+		glVertex2i (x2 + w, y2);
+
+		glTexCoord2i (0, 1);
+		glVertex2i (x2, y2 + h);
+
+		glTexCoord2i (0, 0);
+		glVertex2i (x2, y2);
+		glEnd ();
+	}
 }
--- client/DevEngine/text.h
+++ client/DevEngine/text.h
@@ -1,3 +1,5 @@
+//ignore this for now its crap
+
 /*******************************************************************************
 * Credits:  Andrew Wheeler/Genusis
 *           Freetype library 2
@@ -7,6 +9,7 @@
 
 #include <ft2build.h>
 #include FT_FREETYPE_H
+
 #include "types.h"
 
 typedef struct
@@ -19,13 +22,49 @@ typedef struct
 
 typedef struct
 {
-	char *text;
-	color col;
-	int size;
-	vector2i xy;
-	vector2i hw;
+	GLuint texture;
+	FT_GlyphSlot g;
+}glyph_array;
+
+typedef struct
+{
+	char* data;
+	size_t  count;
+	size_t  size;
+	color col; //color of text
+	int charactersize;
+	vector2i xy; // X location and Y location on screen
+	vector2i hw; //height width
 }text;
 
-void initfont(const char *fontname);
+typedef struct  smallbitmap
+{
+	int             rows;
+	int             width;
+	unsigned char*  buffer;
+} smallbitmap;
+
+typedef struct
+{
+	smallbitmap image; //hold the Bitmap details, pixels, width etc.
+	GLuint tex; //holds the character loaded image.
+	int advancex;	// advance.x
+	int advancey;	// advance.y
+	int bitmapleft;	// bitmap_left;
+	int bitmaptop;	// bitmap_top;
+	int offsetx;	// x offset of glyph in texture coordinates
+	int offsety;	// y offset of glyph in texture coordinates
+} symbol;		// character information
+
+typedef struct
+{
+	GLuint tex;		// texture object Hold the entire font image.
+	int width;			// width of texture in pixels
+	int height;			// height of texture in pixels
+	symbol character[0x0010FFFF]; //holds individual character bitmap data etc.
+}atlas;
+
+void initfont(char *fontname);
+void initatlas(FT_Face face, int height, atlas *cache);
 void rendertext(text *str);
 #endif
