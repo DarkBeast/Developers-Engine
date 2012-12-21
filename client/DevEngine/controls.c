7a0ff2bce0e6f6afbd694062eddef4f9efb006b6
Genusis <lordsatin@hotmail.com>
2012-12-20 21:41:08 -0500

    added in button and label for ui element and is testing.

--- client/DevEngine/controls.c
+++ client/DevEngine/controls.c
@@ -1,47 +1,87 @@
 /*******************************************************************************
 * Credits:  Andrew Wheeler/Genusis
 ******************************************************************************/
-#include "widget.h"
+
 #include "render.h"
 #include "program_path.h"
-#include "globals.h"
-
-void createbutton(widget *button, uint32 x, uint32 y, uint32 height, uint32 width, char* image)
+#include "controls.h"
+#include <stdlib.h>
+#include "widget.h"
+//Buttons
+void createbutton(widget *data, uint16 x, uint16 y, uint16 height, uint16 width, char *image)
 {
-	initwidget(button);
-	button->pos.x = x;
-	button->pos.y = y;
-	button->height = height;
-	button->width = width;
-	button->imgpos.x = 0;
-	button->imgpos.y = 0;
-	button->type = BUTTON;
-	button->draw = &drawbuttons;
+	button *box;
 
-	loadimage(getpath(image), &button->img);
+	initwidget(data);
+	data->pos.x = x;
+	data->pos.y = y;
+	data->height = height;
+	data->width = width;
+	data->imgpos.x = 0;
+	data->imgpos.y = 0;
+	data->type = BUTTON;
+	data->draw = &drawbuttons;
+
+	box = (button *)calloc(1,sizeof(button));
+	box->click = 0;
+
+	data ->control = box;
+	loadimage(getpath(image), &data->img);
 }
 
-void drawbuttons(void * wgt)
+void drawbuttons(void *wgt)
 {
-	widget *data;
-	data = (widget *)wgt;
+	widget *data = (widget *)wgt;
 
-	if (data->mouseover && data->focused)
+	if (ismouseover(data) == TRUE)
 	{
 		if (data->clicked != FALSE)
 		{
-			data->imgpos.y = data->width;
-			drawwidget(data);
+			data->imgpos.x = data->width;
+			data->imgpos.y = 0;
 		}
 		else
 		{
-			data->imgpos.y = 2 * data->width;
-			drawwidget(data);
+			data->imgpos.y = 0;
+			data->imgpos.x = 2 * data->width;
 		}
 	}
 	else
 	{
 		data->imgpos.y = 0;
-		drawwidget(data);
+		data->imgpos.x = 0;
 	}
+
+	drawwidget(data);
+}
+
+//labels
+void createlabel(widget *data, uint16 x, uint16 y, uint8 size, uint8 red, uint8 blue, uint8 green, uint8 alpha, int8 canusemouse, char *labeltext)
+{
+	label *inittext;
+	vector2ui hw;
+
+	initwidget(data);
+	data->pos.x = x;
+	data->pos.y = y;
+	data->imgpos.x = 0;
+	data->imgpos.y = 0;
+	data->type = LABEL;
+	data->draw = &drawlabel;
+	inittext = (label *)calloc(1,sizeof(label));
+	inittext->string = (text *)calloc(1,sizeof(text));
+	settext(inittext->string,x,y,size,red,blue,green,alpha,labeltext);
+	inittext->click = 0;
+	inittext->canusemouse = canusemouse;
+	hw = getmaxstringhw(labeltext,size);
+	data->height = hw.y;
+	data->width = hw.x;
+	data->control = inittext;
+}
+
+void drawlabel(void *wgt)
+{
+	widget *data = (widget *)wgt;
+	label *parent = (label *)data->control;
+	drawtext(parent->string);
 }
--- client/DevEngine/controls.h
+++ client/DevEngine/controls.h
@@ -2,24 +2,35 @@
 * Credits:  Andrew Wheeler/Genusis
 ******************************************************************************/
 
-#ifndef _UICONTROLS_H
-#define _UICONTROLS_H
+#ifndef _CONTROLS_H
+#define _CONTROLS_H
 
+#include "widget.h"
 #include "types.h"
+#include "integer.h"
+#include "text.h"
+#include "globals.h"
 
+#define EMPTY 0;
+#define BUTTON 1;
+#define LABEL 2;
 //only need types for special widgets.
+
 typedef struct
 {//TODO:Update to bit Boolean.
-	uint8 click;
+	int8 click;
 }button;
 
 typedef struct
 {
-	uint8 toggle;
+	int8 canusemouse;
+	int8 click;
+	text *string;
 }label;
 
-void drawbuttons(void * wgt);
+void createbutton(widget *data, uint16 x, uint16 y, uint16 height, uint16 width, char *image);
+void drawbuttons(void *wgt);
 
-#define Empty 0;
-#define BUTTON 1;
+void createlabel(widget *data, uint16 x, uint16 y, uint8 size, uint8 red, uint8 blue, uint8 green, uint8 alpha, int8 canusemouse, char *labeltext);
+void drawlabel(void *wgt);
 #endif
