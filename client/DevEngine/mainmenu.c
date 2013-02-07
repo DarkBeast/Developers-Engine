2ae79a468a3dd67d3bac15d9cbe29dd03b8c5664
Genusis <lordsatin@hotmail.com>
2013-02-06 22:16:28 -0500

    added more widgets for testing

--- client/DevEngine/mainmenu.c
+++ client/DevEngine/mainmenu.c
@@ -7,20 +7,23 @@
 
 widget label1;
 widget button1;
+widget button2;
 widget window1;
+widget window2;
+
 int i = 0;
 
 void main_menu(void)
 {
-	sbool running = TRUE;
+	int running = TRUE;
 	uint32 time;
 	uint32 lpstimer = 0;
 	uint32 lps = 0;
 
 	draw_state_reset();
 
-	while( running){
-		time = (uint32)glfwGetTime();
+	while(running){
+		time = (uint32 )glfwGetTime();
 
 		clear_screen(1,1,1,1);
 
@@ -42,28 +45,35 @@ void main_menu(void)
 		//glfwSleep(.005); //used to save cpu
 
 		// Check if ESC key was pressed or window was closed
-		running = glfwGetWindowParam( GLFW_OPENED);
+		running = glfwGetWindowParam(GLFW_OPENED);
 	}
 }
 
 void init_main_menu(void)
 {
-	create_label(&label1,10,10,0,255,255,255,255,FALSE,"click me!");
+	create_label(&label1,5,5,0,255,255,255,255,FALSE,"click me!");
 	create_button(&button1,3,5,60,100,"image\\buttons.png");
-	create_window(&window1,100,100,150,150,"image\\window.png");
+	create_button(&button2,25,25,60,100,"image\\buttons.png");
+	create_window(&window1,5,5,150,150,"image\\window.png");
+	create_window(&window2,100,100,150,150,"image\\window.png");
 
 	label1.action |= WIDGET_CAN_CLICK_BEHIND;
 	window1.action |= WIDGET_MOVEABLE;
 	window1.action |= WIDGET_CAN_FOCUS;
+	window2.action |= WIDGET_MOVEABLE;
+	window2.action |= WIDGET_CAN_FOCUS;
 
-	widget_add(NULL,&window1,FALSE);
-	widget_add(&window1,&button1,FALSE);
-	widget_add(&button1,&label1,FALSE);
+	widget_add(NULL,&window1);
+	widget_add(NULL,&window2);
+	widget_add(&window1,&button1);
+	widget_add(&window1,&button2);
+
+	widget_add(NULL,&label1);
 
 	button1.mousepress = button1_press;
 }
 
-void button1_press(void *wgt, int button, int pressed)
+void button1_press(widget *wgt, int button, int pressed)
 {
 	label *z = (label *)label1.control;
 	switch(i){
