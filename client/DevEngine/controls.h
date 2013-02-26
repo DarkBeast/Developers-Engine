52ebcbe83b5bf91f46d5fa7f7bfff3df403955e0
Genusis <lordsatin@hotmail.com>
2013-02-26 17:50:08 -0500

    fixed some variable call names, Added progress bar data type<experimental>. removed path form calls now only need to know ID's

--- client/DevEngine/controls.h
+++ client/DevEngine/controls.h
@@ -13,6 +13,9 @@ typedef enum control_types control_types;
 typedef struct label label;
 typedef struct window window;
 typedef struct radio radio;
+typedef struct progressbar progressbar;
+typedef struct control_img control_img;
+
 enum control_types
 {
 	CONTROL_EMPTY = 0,
@@ -35,7 +38,6 @@ enum control_types
 };
 
 //only need structs for special widgets.
-
 struct label{
 	text *string;
 };
@@ -51,28 +53,39 @@ struct radio{
 	uint8 count;
 };
 
-void create_button(widget *data, uint16 x, uint16 y, uint16 height, uint16 width, widget *parent, char *image_path);
+struct progressbar{
+	uint8 value;
+	uint16 size;
+};
+
+void load_control_images(void);
+void set_control_image(widget *control, char *path);
+
+void create_button(widget *control, widget *parent, uint16 x, uint16 y, uint16 height, uint16 width, uint16 sizey, uint16 sizex, uint8 image_id);
 void draw_buttons(void *control);
 void handle_button_click(widget *control, int button, int pressed);
 
-void create_label(widget *data, uint16 x, uint16 y, uint8 size, uint8 red, uint8 blue, uint8 green, uint8 alpha, int8 can_use_mouse, widget *parent, char *label_text);
+void create_label(widget *control, widget *parent, uint16 x, uint16 y, uint8 size, uint8 red, uint8 blue, uint8 green, uint8 alpha, sbool can_use_mouse, char *label_text);
 void draw_label(void *control);
 void handle_label_click(widget *control, int button, int pressed);
 
-void create_window(widget *control, uint16 x, uint16 y, uint16 height, uint16 width, widget *parent, char *image_path);
-void create_window_framed(widget *data, uint16 x, uint16 y, uint16 height, uint16 width, uint16 framex, uint16 framey,uint16 frameh, uint16 framew, widget *parent, char *image_path);
+void create_window(widget *control, widget *parent, uint16 x, uint16 y, uint16 height, uint16 width, uint16 sizey, uint16 sizex, uint8 image_id);
+void create_window_framed(widget *control, widget *parent, uint16 x, uint16 y, uint16 height, uint16 width, uint16 sizey, uint16 sizex, uint16 framex, uint16 framey,uint16 frameh, uint16 framew, uint8 image_id);
 void draw_windows(void *control);
 void handle_window_click(widget *control, int button, int pressed);
 
-void create_checkbox(widget *data, uint16 x, uint16 y, uint16 height, uint16 width, widget *parent, char *image_path);
+void create_checkbox(widget *control, widget *parent, uint16 x, uint16 y, uint16 height, uint16 width, uint16 sizey, uint16 sizex, uint8 image_id);
 void draw_checkbox(void *control);
 void handle_check_click(widget *control, int button, int pressed);
 
-void create_radio(widget *data, uint16 x, uint16 y, uint16 height, uint16 width, sbool istrue, widget *parent, char *image_path);
+void create_radio(widget *control, widget *parent, uint16 x, uint16 y, uint16 height, uint16 width, uint16 sizey, uint16 sizex, sbool istrue, uint8 image_id);
 void draw_radio(void *control);
 void reset_radio(widget *control);
 void link_radio(widget *main, widget *control);
 void handle_radio_click(widget *control, int button, int pressed);
 void resize_radio_list(radio *controls, uint16 size);
 
+void create_progressbar(widget *control, widget *parent, uint16 x, uint16 y, uint16 height, uint16 width, uint16 sizey, uint16 sizex, uint8 value, uint8 image_id);
+void draw_progressbar(widget *control);
+void handle_progressbar_click(widget *control, int button, int pressed);
 #endif
