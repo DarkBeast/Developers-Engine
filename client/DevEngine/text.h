95f2d9cd9b1408c9a3daf0cc066f3f52e9430107
Genusis <lordsatin@hotmail.com>
2013-07-24 22:13:55 -0400

    added VBO array for string rendering and beta vbo functions for testing VBO< currently not working>

--- client/DevEngine/text.h
+++ client/DevEngine/text.h
@@ -35,8 +35,11 @@ struct symbol
 struct text
 {
 	char *data;
+	multi_buffer textbuf; //opengl buffer for text.
 	color col; //color of text
 	vector2ui xy; // X location and Y location on screen
+	uint16 size;
+	uint16 count;
 	uint8 resize;
 };
 
@@ -46,4 +49,8 @@ void text_draw(text *str, widget *parent);
 void text_set(text *text_data, uint16 x, uint16 y, uint8 size, uint8 red,uint8 blue,uint8 green,uint8 alpha, char *string);
 vector2ui text_get_string_hw(char *string, uint8 resize);
 symbol* text_get_character_array(void);
+
+void text_draw_beta(text *str, widget *parent);
+void text_resize_buffer(openext *buffer, uint16 size);
+void create_text_vertex(text *str, widget *control);
 #endif
