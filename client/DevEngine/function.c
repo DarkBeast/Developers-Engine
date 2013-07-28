b43da0c1197a22bb809d5c352c9c116c168995d9
Genusis <lordsatin@hotmail.com>
2013-07-28 16:20:16 -0400

    removed --input for proper power of 2.

--- client/DevEngine/function.c
+++ client/DevEngine/function.c
@@ -3,17 +3,13 @@
 ******************************************************************************/
 
 #include "function.h"
-#include "integer.h"
-
-uint32 program_time;
 
 size_t next_power_of_two(size_t input) {
 	size_t index;
 
-	--input;
-
-	for (index = 1; index < sizeof(size_t); index <<= 1)
+	for (index = 1; index < sizeof(size_t); index <<= 1) {
 		input |= input >> index;
+	}
 
 	return ++input;
 }
