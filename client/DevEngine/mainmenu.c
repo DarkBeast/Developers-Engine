b95d61aa937c65544f5dd6166c223705bdc8d71f
Genusis <lordsatin@hotmail.com>
2013-07-28 16:19:08 -0400

    added up to 7 labels for testing and added in widget_unload to properly unload the widget upon program exit.

--- client/DevEngine/mainmenu.c
+++ client/DevEngine/mainmenu.c
@@ -11,6 +11,12 @@
 
 widget window1;
 widget label1;
+widget label2;
+widget label3;
+widget label4;
+widget label5;
+widget label6;
+widget label7;
 
 image img;
 int i = 0;
@@ -50,13 +56,20 @@ void main_menu(void)
 		// Check if ESC key was pressed or window was closed
 		running = glfwGetWindowParam(GLFW_OPENED);
 	}
+
+	widget_unload(&window1);
 }
 
 void init_main_menu(void)
 {
 	create_window(&window1, NULL, 0, 0, 600, 800, 600, 800, "image\\main.png");
-	create_label(&label1,&window1,5,100,25,255,255,255,255,FALSE,"Hello there this is a VBO test on strings");
-
+	create_label(&label1,&window1,85,300,8,0,0,0,255,FALSE," Hello there this is a VBO test on strings");
+	create_label(&label2,&window1,85,325,8,0,0,0,255,FALSE,"And this is a somewhat paragraph test for");
+	create_label(&label3,&window1,85,350,8,0,0,0,255,FALSE,"Jason whom wanted to test the difference");
+	create_label(&label4,&window1,85,375,8,0,0,0,255,FALSE,"in speed and so far it seems reliable.");
+	create_label(&label5,&window1,85,400,8,0,0,0,255,FALSE,"in speed and so far it seems reliable.");
+	create_label(&label6,&window1,85,425,8,0,0,0,255,FALSE,"in speed and so far it seems reliable.");
+	create_label(&label7,&window1,85,450,8,0,0,0,255,FALSE,"a bleak understanding");
 	window1.action |= WIDGET_CAN_FOCUS;
 	//window1.action |= WIDGET_MOVEABLE;
 	widget_manual_focused(&window1);
