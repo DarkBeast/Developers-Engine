195991dc263432a68fc97a3cad37d02654c9e7a9
Genusis <lordsatin@hotmail.com>
2013-08-31 12:13:51 -0400

    redid the text struct to promote better string drawing for both text box's and labels and redid the font system to load the fonts in a better way to increase lps while drawing and making the fonts take less memory

--- client/DevEngine/text.h
+++ client/DevEngine/text.h
@@ -12,45 +12,67 @@
 #include "integer.h"
 
 typedef struct fonts fonts;
+typedef struct atlas atlas;
 typedef struct symbol symbol;
 typedef struct text text;
 
 struct fonts
 {
-	FT_Face face;
+	FT_Face face; //Font rendering data cache.
 	const char *fontfilename;
+};
+
+struct atlas
+{
+	GLuint tex; //texture
+	int16 h; //texture height
+	int16 w; //texture width
+	symbol *c; //holds individual character bitmap data etc.
 };
 
 struct symbol
 {
-	FT_Glyph glyph;
-	FT_BitmapGlyph  glyph_bitmap;
-	uint16 x;
-	uint16 y;
-	int16 advancex;
-	int16 advancey;
+	float width; // bitmap.width;
+	float height; // bitmap.height;
+	float left; // bitmap_left;
+	float top; // bitmap_top;
+	float x; //texture x
+	float y; //texture y
+	float ax; //advance x
+	float ay; //advance y
+	uint16 mheight; //metric height, space between lines.
 };		// character information
 
 struct text
 {
-	char *data;
-	multi_buffer textbuf; //opengl buffer for text.
-	color col; //color of text
-	vector2ui xy; // X location and Y location on screen
-	uint16 size;
-	uint16 count;
-	uint16 max;
-	uint8 resize;
+	char *data; //the entire string.
+	char *drawable; //whats being drawn if supported.
+	uint16 size; //string max size.
+	uint16 count; //string count.
+	mbuffer buf; //opengl buffer.
+	color col; //color of text.
+	vector2ui xy; // X and Y location on screen.
+	uint16 width; //width of control.
+	uint16 height; //height of control.
+	uint16 lines; // amount of lines;
+	uint16 maxchars; //maximum amount of characters allowed per line.
+	uint8 offsetx; //rendering offset x.
+	uint8 offsety; //rendering offset y.
+	uint16 textheight; //text height.
+	uint16 textwidth; //text width.
+	atlas *font; //font used for drawing.
 };
 
 void text_init_font(char *fontname);
 void text_init_atlas(void);
 void text_draw(text *str);
-void text_set(text *text_data, uint16 x, uint16 y, uint8 size, uint8 red,uint8 blue,uint8 green,uint8 alpha, char *string);
-vector2ui text_get_string_hw(char *string, uint8 resize);
-symbol* text_get_character_array(void);
+void text_set(text *data, uint16 x, uint16 y, uint16 width, uint16 height, uint8 offsetx, uint8 offsety, uint32 maxchars, uint8 fontid, uint8 red, uint8 blue, uint8 green, uint8 alpha, char *string);
+atlas *get_atlas(uint8 fontid);
 
 void text_resize_buffer(vertex_array **buffer, size_t size);
 void create_text_vertex(text *str, widget *control);
 void text_position_update(text *str, widget *parent);
+void string_resize(text *str, uint32 size);
+void textbox_text_update(text *str, widget *control);
+
 #endif
