48224b25b9cc3fa8a3a8a853a5eb7529b7f1b663
Genusis <lordsatin@hotmail.com>
2013-12-15 23:21:58 -0500

    updated linsocket struture

--- client/Source/linsocket.h
+++ client/Source/linsocket.h
@@ -10,12 +10,6 @@ struct desocket
 {
 	int socket; // Socket descriptor
 	//struct sockaddr_in host_addrress;
-	unsigned short host_port;
-	char *host_ip;
-	char buffer[SOCKET_REC_SIZE];
-	int buffer_size; //length of the packet
-	int bytes_read;
-	int total_bytes_read;
 };
 
 #endif
