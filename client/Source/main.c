25c5c87de41f85ab77b9cb5a49df67f2666a8556
Genusis <lordsatin@hotmail.com>
2013-12-15 23:21:46 -0500

    added in a unload function.

--- client/Source/main.c
+++ client/Source/main.c
@@ -5,18 +5,15 @@
 #include <GL/glew.h>
 #include <glfw3.h>
 #include "input.h"
-#include "program_path.h"
+#include "path.h"
 #include "render.h"
 #include "text.h"
-#include "mainmenu.h"
 #include "bool.h"
-#include "controls.h"
-#include "error.h"
-
+#include "main.h"
 #include "tinycthread.h"
 #include "handlepackets.h"
 #include "winsocket.h"
-#include "clienttcp.h"
+#include "general.h"
 
 mtx_t gmutex;
 cnd_t gcond;
@@ -28,12 +25,12 @@ int main(void)
 	mtx_init(&gmutex, mtx_plain);
 	cnd_init(&gcond);
 
-	get_program_directory();//gets the programs path.
+	init_path(); //allocates the path size.
 
 	init_screen(800,600,NULL,NULL);//initializes the program window.
 
 	glfwSetWindowCloseCallback(get_the_window(),handle_window_close);//checks if the window closed or not.
-	glfwSetWindowSizeCallback(get_the_window(),handle_resize);//handles Window resize calls
+	//glfwSetWindowSizeCallback(get_the_window(),handle_resize);//handles Window resize calls
 	glfwSetCharCallback(get_the_window(),handle_char_callback);//handles character presses
 	glfwSetKeyCallback(get_the_window(),handle_key_callback);
 	glfwSetCursorPosCallback(get_the_window(),handle_mouse_pos);//handles mouse move events
@@ -43,19 +40,22 @@ int main(void)
 
 	widget_init_system();//initializes the widget system for storing widgets.
 
-	//init_socket();
-	//init_packets();
-	//sendlogin("me","you");
-	init_main_menu();//initializes the first menu for use.
-	//thrd_create(&t1, socket_loop, (void*)0);
+	init_packets();
+	init_client();
 	initsocket();
 	socketconnect();
-	main_menu();//Start the First Menu, Menu main loop.
+	thrd_create(&t1, socketlisten, (void*)0);
 
+	menustate();
+	unload_functions();
+}
+
+void unload_functions(void)
+{
 	glfwDestroyWindow(get_the_window());
 	glfwTerminate();// Close window and terminate GLFW
-	path_destroy();
-	//destory_client_socket();
+	path_destroy();//De-allocates the path size.
+	unloadsocket();
 	mtx_destroy(&gmutex);
 	cnd_destroy(&gcond);
 	exit(TRUE);// Exit program
