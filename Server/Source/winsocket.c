5b752e740dd4423801b90e54f316804d41e7acf1
Genusis <lordsatin@hotmail.com>
2014-02-25 19:51:05 -0500

    Added check for testing client side to server side stuff.

--- Server/Source/winsocket.c
+++ Server/Source/winsocket.c
@@ -86,6 +86,7 @@ void socket_read(struct bufferevent *bev, void *index)
 
 		handle_data(&buffer, bev, index);
 	}
+	puts("lost connection?");
 }
 
 void socket_event(struct bufferevent *bev, short events, void *index)
