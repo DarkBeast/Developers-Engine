81856665aaf37a244b717a66739b4294a6867ec5
Genusis <lordsatin@hotmail.com>
2014-03-19 00:12:32 -0400

    Made encryption key bigger for full login and new account string encryption.

--- Server/Source/globals.h
+++ Server/Source/globals.h
@@ -51,9 +51,9 @@
 #define MAX_LEVELS   100
 #define MAX_JOBS  3
 
-//encrypt key must be string!
+//encrypt key must be string and be at minimal 32 characters long
 //Server and Client encrypt key must match.
-#define ENCRYPT_KEY  "Aab456"
+#define ENCRYPT_KEY  "Aab456sdfg5gf89d5h41r68d49f8gh4rhrt651dr8tg41d6h5g1h"
 
 /* network defines*/
 #define SERVER_IP     "localhost"
