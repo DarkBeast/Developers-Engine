3be05198699f8c4b26cb9e13a495022c774afc0f
Genusis <lordsatin@hotmail.com>
2014-03-19 00:13:22 -0400

    Fixed Comp_strings.

--- Server/Source/function.c
+++ Server/Source/function.c
@@ -21,9 +21,8 @@ size_t next_power_of_two(size_t input)
 {
 	size_t index;
 
-	for (index = 1; index < sizeof(size_t); index <<= 1) {
+	for (index = 1; index < sizeof(size_t); index <<= 1)
 		input |= input >> index;
-	}
 
 	return ++input;
 }
@@ -32,19 +31,25 @@ sbool comp_str(char *string1, char *string2)
 {
 	uint32 i = 1;
 
+	if(string1 == NULL || string2 == NULL)
+		return FALSE;
+
+	if(*(string1) != *(string2))
+		return FALSE;
+
 	while(*string2){
-		if(*(string1 + i) == *(string2++)){
-			if(*(string1 + i + 1) == ' ' || *(string1 + i + 1) == '\n' || *(string1 + i + 1) == NULL ){
+		if(*(string1 + i) == *(string2 + i)){
+			if(*(string1 + i + 1) == ' ' || *(string1 + i + 1) == '\n' || *(string1 + i + 1) == NULL )
 				return TRUE;
-			}
+
 			i++;
 		}
 		else{
-			if(*(string1 + i) == ' ' || *(string1 + i) == '\n'){
+			if(*(string1 + i) == ' ' || *(string1 + i) == '\n')
 				return TRUE;
-			}
+
 			return FALSE;
-		}	
+		}
 	}
 
 	return FALSE;
